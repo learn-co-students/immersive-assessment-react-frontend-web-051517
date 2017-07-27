29fc53841bd02c22962802e1831ba42cd87f2456
wkhearn <wkhearn@gmail.com>
2017-07-27 11:33:06 -0400

    Done.

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,10 @@
 import React from 'react'
 
-const Search = () => {
+
+const Search = props => {
   return (
     <div className="ui huge fluid icon input">
-      <input
-        type="text"
-        placeholder={"Search your Recent Transactions"}
-      />
+      <input type="text" placeholder={"Search your Recent Transactions"} onChange={props.handleSearch} />
       <i className="circular search link icon"></i>
     </div>
   )
