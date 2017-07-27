69bf2a8304e127a06b9cbe890e8fa211fdc43349
Pat Rocchio <procchio6@gmail.com>
2017-07-27 10:35:18 -0400

    Done

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,12 +1,14 @@
 import React from 'react'
 
-const Transaction = () => {
+const Transaction = (props) => {
+  const {posted_at, amount, category, description} = props.transaction
+  
   return (
     <tr>
-      <td>{"...your code here"}</td>
-      <td>{"...your code here"}</td>
-      <td>{"...your code here"}</td>
-      <td>{"...your code here"}</td>
+      <td>{posted_at}</td>
+      <td>{description}</td>
+      <td>{category}</td>
+      <td>{amount}</td>
     </tr>
   )
 }
