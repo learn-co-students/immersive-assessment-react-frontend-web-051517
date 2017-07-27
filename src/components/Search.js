885b1243a481670ae91da38fea91f4ebffd9e1fc
rkoko <rinda.ko@gmail.com>
2017-07-27 11:33:38 -0400

    Done.

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
-const Search = () => {
+const Search = (props) => {
   return (
     <div className="ui huge fluid icon input">
       <input
         type="text"
+        onChange={props.handleChange}
+        value={props.searchTerm}
         placeholder={"Search your Recent Transactions"}
       />
       <i className="circular search link icon"></i>
--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -1,6 +1,9 @@
 import React from 'react'
+import Transaction from './Transaction'
 
-const TransactionsList = () => {
+const TransactionsList = (props) => {
+
+  const filteredList = props.transactions.filter(transaction => transaction.description.toLowerCase().includes(props.searchTerm.toLowerCase()) || transaction.category.toLowerCase().includes(props.searchTerm.toLowerCase()))
 
   return (
     <table className="ui celled striped padded table">
@@ -28,7 +31,7 @@ const TransactionsList = () => {
           </th>
         </tr>
 
-        {"... your code here..."}
+        {filteredList.map(transaction => <Transaction transaction={transaction} />)}
 
       </tbody>
     </table>
